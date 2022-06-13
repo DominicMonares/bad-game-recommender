5df29d316895ff6515d2f82cb7c3e1a77fa68242
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-12 20:40:21 -0700

    implement useEffect

--- src/native/App.js
+++ src/native/App.js
@@ -1,38 +1,30 @@
 import React, { useState, useEffect } from 'react';
-import type {Node} from 'react';
-import {
-  Button,
-  SafeAreaView,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, View} from 'react-native';
 
 import fetchGameData from '../shared/services/gameData';
 
-const App: () => Node = () => {
+const App = () => {
   const [page, setPage] = useState(1);
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    const gameData = async () => {
+      setGames(await fetchGameData(page));
+    }
+
+    gameData();
+  }, [])
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.response}>Native Test</Text>
-      <Button
-        onPress={() => fetchGameData(page)}
-        title={'Fetch Game Data'}
-        color="purple"
-      />
+
     </SafeAreaView>
   );
-};
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
-  },
-  response: {
-    textAlign: 'center',
-    marginTop: 'auto',
-    marginBottom:'auto'
   }
 });
 
