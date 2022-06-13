158fed2fcfc15712a91dfdc62ffd5b560ed2bb1d
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-12 20:34:57 -0700

    refactor fetchGameData

--- src/shared/services/gameData.js
+++ src/shared/services/gameData.js
@@ -1,9 +1,11 @@
-const fetchGameData = (page) => {
-  return fetch(`http://34.198.169.107:8080/games/`, {
+const fetchGameData = async (page) => {
+  const gameData = await fetch(`http://34.198.169.107:8080/games/${page}`, {
     method: 'GET'
   })
     .then(res => res.json())
     .catch(err => { throw err });
+
+  return gameData;
 }
 
 export default fetchGameData;
