d61dbab55fad7e76015a63e7c9942d3de3faa490
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-13 22:07:21 -0700

    implement shadow styling

--- src/native/components/Game/Cover.js
+++ src/native/components/Game/Cover.js
@@ -6,16 +6,46 @@ const Cover = (props) => {
   const rating = props.rating;
 
   return (
-    <View>
-      <Image
-        style={styles.cover}
-        source={{uri: url}}
-      />
+    <View style={styles.blContainer}>
+      <View style={styles.brContainer}>
+        <View style={styles.tlContainer}>
+          <View style={styles.trContainer}>
+            <Image
+              style={styles.cover}
+              source={{ uri: url }}
+            />
+          </View>
+        </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  blContainer: {
+    shadowColor: 'black',
+    shadowOffset: {width: -4, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+  brContainer: {
+    shadowColor: 'black',
+    shadowOffset: {width: 4, height: 4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+  tlContainer: {
+    shadowColor: 'black',
+    shadowOffset: {width: -4, height: -4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
+  trContainer: {
+    shadowColor: 'black',
+    shadowOffset: {width: 4, height: -4},
+    shadowOpacity: 0.4,
+    shadowRadius: 3,
+  },
   cover: {
     resizeMode: 'contain',
     width: 264,
@@ -23,10 +53,7 @@ const styles = StyleSheet.create({
     marginLeft: 'auto',
     marginRight: 'auto',
     backgroundColor: 'black',
-    shadowColor: 'black',
-    shadowOffset: {width: -2, height: 4},
-    shadowOpacity: 0.2,
-    shadowRadius: 3,
+    borderRadius: 10
   }
 });
 
