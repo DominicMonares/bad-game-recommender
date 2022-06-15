54d79a6bdfe20b820f5e1db725d694fd5a512008
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-14 17:32:31 -0700

    implement native tests

---
+++ tests/native/native.test.js
@@ -0,0 +1,52 @@
+/**
+ * @format
+ */
+
+import 'react-native';
+import React from 'react';
+import { create } from 'react-test-renderer';
+import { act } from '@testing-library/react-native';
+
+import App from '../../src/native/App';
+import { logoStyle } from './sampleData'
+
+jest.mock('../../src/shared/assets/bgr-logo.png');
+
+describe('Native Tests', () => {
+
+  describe('App', () => {
+    it('renders App correctly', () => {
+      const component = create(<App />);
+      const tree = component.toJSON();
+      const logo = tree['children'][1]['children'][0]['children'][0];
+      console.log('LOGO ', logo)
+
+      expect(tree.type).toBe('View');
+      expect(tree.props.style).toStrictEqual({ flex: 1 });
+      expect(tree.children.length).toBe(2);
+      expect(tree['children'][0]['type']).toBe('View');
+      expect(tree['children'][1]['type']).toBe('RCTScrollView');
+      expect(logo.type).toBe('Image');
+      expect(logo.props).toStrictEqual(logoStyle);
+
+    });
+
+    // it('calls useEffect on App load', () => {
+    //   let component;
+    //   act(() => {
+    //     component = create(<App />);
+    //   });
+
+    //   const tree = component.toJSON();
+    //   const scrollView = tree['children'][1]['children'][0]['children'];
+    //   const logo = scrollView[0];
+    //   console.log('COMP ', scrollView);
+    // });
+  });
+
+
+
+
+
+
+});
---
+++ tests/native/sampleData.js
@@ -0,0 +1,14 @@
+const logoStyle = {
+  flex: 1,
+  resizeMode: 'contain',
+  width: 200,
+  height: 119,
+  marginLeft: 'auto',
+  marginRight: 'auto',
+  marginBottom: 44,
+  marginTop: 40
+}
+
+module.exports = {
+  logoStyle: logoStyle
+}
