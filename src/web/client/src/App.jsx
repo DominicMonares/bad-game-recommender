dd73b7f221dbb7b9437b85dbc58c86e3d492d4de
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-16 02:42:29 -0700

    implement App

--- src/web/client/src/App.jsx
+++ src/web/client/src/App.jsx
@@ -1,21 +1,44 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
 
-import logo from '../../../shared/assets/bgr-logo.png';
+import { store } from '../../../../shared/store';
+import { updateGames } from '../../../../shared/actions';
+
+import logo from '../../../../shared/assets/bgr-logo.png';
 import Game from './components/Game';
 import PageButtons from './components/PageButtons';
 
+import fetchGameData from '../../../../shared/services/gameData';
 
+const AppWrapper = () => {
+  return (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  )
+}
 
 const App = () => {
-  const [page, setPage] = useState(1);
-  const [games, setGames] = useState([]);
+  const page = useSelector(state => state.page);
+  const games = useSelector(state => state.games);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const getInitialGames = async () => {
+      const gameData = await fetchGameData(page);
+      dispatch(updateGames(gameData));
+    }
+
+    getInitialGames();
+  }, [])
 
   return (
     <div className="app_container">
       <img className="logo" src={logo} />
-      <Game game={game} />
+      {games.map(g => <Game key={g.name} game={g} />)}
+      <PageButtons />
     </div>
   )
 }
 
-export default App;
+export default AppWrapper;
