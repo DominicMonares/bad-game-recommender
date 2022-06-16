fe490689981327227a185ff5bf7c5a12ce4e345f
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-15 21:42:08 -0700

    implement stylesheet

--- src/web/client/src/App.jsx
+++ src/web/client/src/App.jsx
@@ -5,7 +5,7 @@ import logo from '../../../shared/assets/bgr-logo.png';
 const App = () => {
   return (
     <div>
-      <img src={logo} />
+      <img className="logo" src={logo} />
       <h1>WEB TEST</h1>
     </div>
   )
