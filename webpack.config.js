8665cc78bda2211b647c4fb82303680532c558e5
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-15 21:09:31 -0700

    add options for file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,11 +20,10 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
+        loader: 'file-loader',
+        options: {
+          name: 'assets/logo.png',
+        }
       }
     ]
   }
