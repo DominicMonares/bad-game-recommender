d24e719cc96c789f7774ff17bc7b18dfbf65b2d2
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-15 23:00:58 -0700

    save progress

--- src/web/client/src/App.jsx
+++ src/web/client/src/App.jsx
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import logo from '../../../shared/assets/bgr-logo.png';
+import Game from './components/Game';
+import PageButtons from './components/PageButtons';
+
+
 
 const App = () => {
+  const [page, setPage] = useState(1);
+  const [games, setGames] = useState([]);
+
   return (
-    <div>
+    <div className="app_container">
       <img className="logo" src={logo} />
-      <h1>WEB TEST</h1>
+      <Game game={game} />
     </div>
   )
 }
