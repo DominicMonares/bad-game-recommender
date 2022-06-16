0bd2542f17552332322021fdf614254c588df1c4
Dominic Monares <78285106+DominicMonares@users.noreply.github.com>
2022-06-15 20:22:46 -0700

    implement axios test

---
+++ tests/shared/sampleData.js
@@ -0,0 +1,56 @@
+const games = [
+  {
+    id: 3046,
+    genres: ['Shooter', 'Adventure', 'Indie'],
+    name: "Day One: Garry's Incident",
+    rating: 10.0019659983726,
+    url: 'https://www.igdb.com/games/day-one-garry-s-incident',
+    cover_url: 'https://images.igdb.com/igdb/image/upload/t_cover_big/oxeeg8wve9gog5bnndrr.jpg'
+  },
+  {
+    id: 2571,
+    genres: ['Fighting'],
+    name: 'Dragonball Evolution: The Game',
+    rating: 10.025032968696,
+    url: 'https://www.igdb.com/games/dragonball-evolution-the-game',
+    cover_url: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co3tsf.jpg'
+  },
+  {
+    id: 8534,
+    genres: ['Adventure'],
+    name: "Zelda's Adventure",
+    rating: 10.0675406932677,
+    url: 'https://www.igdb.com/games/zeldas-adventure',
+    cover_url: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co1wsk.jpg'
+  },
+  {
+    id: 2649,
+    genres: ['Shooter', 'Simulator', 'Adventure', 'Indie'],
+    name: 'Starforge',
+    rating: 10.1775779095944,
+    url: 'https://www.igdb.com/games/starforge',
+    cover_url: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co4tj2.jpg'
+  },
+  {
+    id: 3778,
+    genres: ['Racing'],
+    name: 'FlatOut 3: Chaos & Destruction',
+    rating: 10.2667478390445,
+    url: 'https://www.igdb.com/games/flatout-3-chaos-destruction',
+    cover_url: 'https://images.igdb.com/igdb/image/upload/t_cover_big/co2f6g.jpg'
+  }
+]
+
+const game1 = {
+  id: 3046,
+  genres: ['Shooter', 'Adventure', 'Indie'],
+  name: "Day One: Garry's Incident",
+  rating: 10.0019659983726,
+  url: 'https://www.igdb.com/games/day-one-garry-s-incident',
+  cover_url: 'https://images.igdb.com/igdb/image/upload/t_cover_big/oxeeg8wve9gog5bnndrr.jpg'
+}
+
+module.exports = {
+  games: games,
+  game1: game1
+}
--- tests/shared/shared.test.js
+++ tests/shared/shared.test.js
@@ -1,10 +1,15 @@
 import React, { useEffect, useRef, useState } from 'react';
 import fetchGameData from '../../src/shared/services/gameData';
 
+import { games } from './SampleData';
+
 describe('Shared', () => {
 
   it('should fetch game data', async () => {
-    console.log('TEST ', await fetchGameData(1));
+    const response = await fetchGameData(1);
+
+    expect(response.length === 5);
+    expect(response).toStrictEqual(games);
   });
 
 });
